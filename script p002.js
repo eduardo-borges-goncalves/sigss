66ebdaf1c77e00ccd3d9ff1838d6eeaec4611591
Eduardo Borges <92747098+eduardo-borges-goncalves@users.noreply.github.com>
2021-11-19 20:53:47 -0300

    Add files via upload
    First step of the initial area. Side bar and menu bar. This day was so terrible hard.

--- script p002.js
+++ script p002.js
@@ -2,6 +2,7 @@
 let logUser = JSON.parse(localStorage.getItem('logUser') )
 let welcomeUser = document.querySelector("#welcomeUser")
 
+let menu = document.getElementById('menu')
 
 
 
@@ -18,3 +19,37 @@ function sair () {
   localStorage.removeItem('logUser')
   window.location.href = 'index.html'
 }
+
+
+class MobileNavBar  {
+  constructor (mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = 'active';
+
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+      this.navList.classList.toggle(this.activeClass);
+  }
+  addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+  init() {
+      if (this.mobileMenu) {
+          this.addClickEvent()
+      }
+      return this;
+  }
+}
+
+
+const mobileNavBar = new MobileNavBar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+
+mobileNavBar.init()
