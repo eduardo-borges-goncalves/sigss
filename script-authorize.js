836225fca5ba797bef9d444444fac8bbc2bea9b9
Eduardo Borges <92747098+eduardo-borges-goncalves@users.noreply.github.com>
2021-12-01 17:12:11 -0300

    Add files via upload
    correcting js script authorize

--- script-authorize.js
+++ script-authorize.js
@@ -7,7 +7,60 @@ let tipoDeExame = document.getElementById('tipoDeExameDiv')
 let validate = false
 let validateExam = false
 
+let menu = document.getElementById('menu')
 
+let navList = document.getElementById('nav-list')
+let menuOn = false
+
+let subList = document.getElementById('sub-list')
+let subListOn = false
+let menuLevelOne = document.getElementById('menu-level-one')
+
+if (localStorage.getItem('token') == null) {
+  window.alert('Você precisa estar logado para acessar essa página')
+  window.location.href = 'login.html'
+} else {
+  welcomeUser.innerHTML = `Bem vindo ${logUser.name.toUpperCase()}!`
+}
+
+function sair () {
+  localStorage.removeItem('token')
+  localStorage.removeItem('logUser')
+  window.location.href = 'index.html'
+}
+
+function mobileMenu(){
+ 
+  if(menuOn == false) {
+    navList.setAttribute('style', "transform: translateX(0)")
+    menuOn = true;
+  } 
+  
+  else if (menuOn == true) {
+    navList.setAttribute('style', "transform: translateX(-150%")
+    menuOn = false;
+    menuLevelOne.setAttribute("style", "height: 12vh")
+    subList.setAttribute('style', "transform: translateX(-150%)")
+    subListOn = false;
+  }
+}
+
+function levelOneMenu(){
+ 
+  if(subListOn == false) {
+    menuLevelOne.setAttribute("style", "height: 40vh")
+    subList.setAttribute('style', "transform: translateX(0)")
+    subListOn = true;
+  } 
+  
+  else if (subListOn == true) {
+    menuLevelOne.setAttribute("style", "height: 12vh")
+    subList.setAttribute('style', "transform: translateX(-150%)")
+    subListOn = false;
+  }
+}
+
+// authorize js
 
 function teste() {
     console.log(consulta)
